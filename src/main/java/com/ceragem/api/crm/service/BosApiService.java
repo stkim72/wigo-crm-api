c0539a70be08e056becc3c8a893ecf7bcab8e9db
김성태 <stkim@wigoai.com>
2023-01-31 09:50:37 +0900

    bos api

--- src/main/java/com/ceragem/api/crm/service/BosApiService.java
+++ src/main/java/com/ceragem/api/crm/service/BosApiService.java
@@ -10,7 +10,6 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ceragem.api.base.util.Utilities;
